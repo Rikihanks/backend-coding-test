58b71ed9f6894e04c3cb44aa7579683e12708292
rikihanks <richard_hanks@outlook.com>
2022-02-25 14:02:11 +0000

    fix: Saving tasks with subtaks now works properly

--- src/main/java/com/example/demo/task/entities/SubtaskEntity.java
+++ src/main/java/com/example/demo/task/entities/SubtaskEntity.java
@@ -1,13 +1,18 @@
 package com.example.demo.task.entities;
 
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.CascadeType;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
 public class SubtaskEntity extends TaskEntity {
 
-    @ElementCollection
+    @OneToMany(cascade = CascadeType.ALL)
     private List<TaskEntity> tasks;
 
     public SubtaskEntity(List<TaskEntity> tasks) {
